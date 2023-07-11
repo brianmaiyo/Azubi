e994b528bd217e5a7d6834715950936c3160926e
brianmaiyo <brian1maiyo@gmail.com>
2023-07-11 17:33:24 +0300

    Initial commit for Tip calculator

---
+++ TipCalculator.py
@@ -0,0 +1,7 @@
+
+amount= float(input('Enter the charge for the food \n'))
+result1= round(0.18*amount,2)
+result2= round(0.07*amount,2)
+print(f'Tip is = ${result1}')
+print(f'Sales tax = ${result2}')
+print(f'Grand total =${amount+result1+result2}')
