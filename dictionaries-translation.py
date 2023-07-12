fc3a945ce874fc193de915b933912c1b1f1f5ad5
brianmaiyo <brian1maiyo@gmail.com>
2023-07-12 15:17:38 +0300

    Translation exercise using dictionaries

--- dictionaries-translation.py
+++ dictionaries-translation.py
@@ -15,3 +15,6 @@ german_input =input('Enter the corresponding color in German: \n')
 colors[english_input]=german_input
 
 print( colors)
+
+query = input('which color should be translated?: \n')
+print(f'The German word for {query} is {colors[query]}')
