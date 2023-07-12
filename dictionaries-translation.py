de872a87c169956c48b54329a352691676735c7d
brianmaiyo <brian1maiyo@gmail.com>
2023-07-12 14:54:45 +0300

    Tranlation excercise using dictionaries

---
+++ dictionaries-translation.py
@@ -0,0 +1,17 @@
+#creted a dictionary which consisted of colors red,green, blue, yellow as the keys 
+#and the german translation rot,grun,blau,gelb as values.
+#Using the input, asked  for another english color 
+#and its german translation and added the pair to the dictionary
+
+colors={}
+colors['red']='rot'
+colors['green']='grun'
+colors['blue']='blau'
+colors['yellow']='gelb'
+print( colors)
+
+english_input =input('Enter another color in English: \n')
+german_input =input('Enter the corresponding color in German: \n')
+colors[english_input]=german_input
+
+print( colors)
